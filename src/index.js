f131d5b68f8691641c11b4adb20fad67ca13c198
ittyi <62760395+ittyi@users.noreply.github.com>
2021-08-22 10:57:28 +0900

    追加フォーム機能作成（途中）

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // text boxの値を取得したのち、初期化
+  const inputText = document.getElementById("add_text").value;
+  document.getElementById("add_text").value = "";
+
+  // li作成
+  const li = document.createElement("li");
+
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list_row";
+
+  // p作成
+  const p = document.createElement("p");
+  p.className = "text_content";
+  p.innerText = inputText;
+
+  // liの子要素に各要素を設定
+  li.appendChild(div);
+
+  // divの子要素に各要素を設定
+  div.appendChild(p);
+  console.log(li);
+
+  // 作業中TODOに追加
+  document.getElementById("working_todo").appendChild(li);
+};
+
+document
+  .getElementById("add_button")
+  .addEventListener("click", () => onClickAdd());
